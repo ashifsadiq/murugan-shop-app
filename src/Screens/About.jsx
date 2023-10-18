cf45ad1d65762b2df01da7559d41933326bb4670
durai08 <duraichandran08@gmail.com>
2023-10-18 08:24:55 +0530

    width

--- src/Screens/About.jsx
+++ src/Screens/About.jsx
@@ -15,39 +15,39 @@ const AboutPage = () => {
 
   return (
     <div className="about-page-container">
-      <div className="fireworks-container">
-        <Fireworks />
-      </div>
-      <div className="about-content">
-        <h3 className="about-heading">About us</h3>
-        <img src={img1} alt="About Us" className="about-image" />
-        <div>
-          <h1 className="bi bi-shop"> BRANCH</h1>
-          <ul className="custom-bullet-list">
-            <li>GANESHAPURAM</li>
-            <li>KOVILPALAYAM &</li>
-            <li>NEELAMBUR</li>
-          </ul>
-          <div className="contact">
+    <div className="fireworks-container">
+      <Fireworks />
+    </div>
+    <div className="about-content">
+      <h3 className="about-heading">About us</h3>
+      <img src={img2} alt="About Us" className="about-image" /> {/* Add this line */}
+      <div>
+        <h1 className="bi bi-shop"> BRANCH</h1>
+        <ul className="custom-bullet-list">
+          <li>GANESHAPURAM</li>
+          <li>KOVILPALAYAM &</li>
+          <li>NEELAMBUR</li>
+        </ul>
+        <div className="contact">
           <h5 className="contact_content">
             <i className="bi bi-phone" style={{ fontWeight: 'bold'   }}></i> CONTACT: 80126 41274 |<br /> 96886 20121 |
             94888 83399
           </h5>
         </div>
-        </div>
-      
-        <h5 className="about-info">
-          <a
-            href="https://www.instagram.com/muruganpattasukadai/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="instagram-link"
-          >
-            <i className="bi bi-instagram instagram-icon"></i> Instagram
-          </a>
-        </h5>
       </div>
+    
+      <h5 className="about-info">
+        <a
+          href="https://www.instagram.com/muruganpattasukadai/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="instagram-link"
+        >
+          <i className="bi bi-instagram instagram-icon"></i> Instagram
+        </a>
+      </h5>
     </div>
+  </div>
   );
 };
 
